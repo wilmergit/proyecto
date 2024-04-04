7b4f9a0ac5d349a6a821c5b1d1ede60bab038bd3
wilmergit <wilmerzeasrodriguez@gmail.com>
2024-04-04 17:10:45 +0100

    first commit

---
+++ Exposicion/exposicion/src/pages/home/Home.jsx
@@ -0,0 +1,13 @@
+import './Home.css'
+
+function Home(){
+    return (
+        <>
+            <div className="home-intro">
+                <img src="/images/fondo-proyecto.webp" alt="imagen" />
+            </div>
+        </>
+    )
+}
+
+export default Home;
