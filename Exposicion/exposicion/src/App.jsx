88e625a871e7f9f2b602164301bff7fbdfc467ec
wilmergit <wilmerzeasrodriguez@gmail.com>
2024-04-05 18:28:22 +0100

    second commit

--- Exposicion/exposicion/src/App.jsx
+++ Exposicion/exposicion/src/App.jsx
@@ -1,12 +1,16 @@
 import './App.css'
 import Home from './pages/home/Home'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
 
   return (
-    <>
-      <Home/>
-    </>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/home' element={<Home/>}/>
+        </Routes>
+      </BrowserRouter>
   )
 }
 
--- Exposicion/exposicion/src/pages/home/Home.jsx
+++ Exposicion/exposicion/src/pages/home/Home.jsx
@@ -4,6 +4,9 @@ function Home(){
     return (
         <>
             <div className="home-intro">
+                <div className='home-header'>
+                    <p>HOLA PRUEBA</p>
+                </div>
                 <img src="/images/fondo-proyecto.webp" alt="imagen" />
             </div>
         </>
