57da9bc0759cc7d48dcf80ddfda1a726aacc91d9
wilmergit <wilmerzeasrodriguez@gmail.com>
2024-04-09 19:29:42 +0100

    remote commit

--- Exposicion/src/components/image-gallery/Gallery.jsx
+++ Exposicion/src/components/image-gallery/Gallery.jsx
@@ -2,9 +2,23 @@ import "./Gallery.css"
 
 function Gallery(){
     return (
-        <div className="gallery-container">
-            <img src="./images/sphinx-tanis-home.JPG" alt="sphinx"/>
-            <img src="./images/victoria-home.png" alt="victoria"/>
+        <div className="gallery-carousel">
+            <div className="carousel-list">
+                <div className="list-item">
+                    <img src="./images/sphinx.jpg" alt="sphinx" />
+                    <div className="item-content">
+                        <div className="item-author">Autor</div>
+                        <div className="item-title">Titulo</div>
+                        <div className="item-category">Categoria</div>
+                        <div className="item-description">
+                            Esto es un texto de prueba para la descripcion.
+                        </div>
+                        <div className="buttons">
+                            <button></button>
+                        </div>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
--- Exposicion/src/pages/home/Home.jsx
+++ Exposicion/src/pages/home/Home.jsx
@@ -1,4 +1,5 @@
 import Gallery from "../../components/image-gallery/Gallery";
+import Menu from "../../components/menu/Menu";
 import "./Home.css"
 
 function Home(){
@@ -9,10 +10,7 @@ function Home(){
                 <img className="fondo" src="./images/fondo.jpg" alt="" />
                 <div className="home-header">
                     <h1 className="home-header-title">EXPO</h1>
-                    <div className="home-header-options">
-                        <h1>About us</h1>
-                        <h1>Contact us</h1>
-                    </div>
+                    <Menu/>
                 </div>
                 <h2 className="slogan">ENJOY THE VIEW</h2>
             </div>
