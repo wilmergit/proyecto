98f9515df356474e2b596891823f695d7cc9cdc7
wilmergit <wilmerzeasrodriguez@gmail.com>
2024-04-11 16:54:59 +0100

    jodienda

--- Exposicion/src/components/image-gallery/Gallery.jsx
+++ Exposicion/src/components/image-gallery/Gallery.jsx
@@ -1,8 +1,23 @@
 import "./Gallery.css"
+import { useState } from "react";
 
-function Gallery(){
+
+function Gallery() {
+
+    const [stateGallery, setStateGallery] = useState('gallery-carousel');
+
+    const changeStateGallery = () => {
+        if (stateGallery == 'gallery-carousel') {
+            setStateGallery('gallery-carousel next');
+        }
+        else {
+            setStateGallery('gallery-carousel');
+        }
+    }
+
     return (
-        <div className="gallery-carousel">
+
+        <div onClick={changeStateGallery} className={stateGallery}>
             <div className="carousel-list">
                 <div className="list-item">
                     <img src="./images/sphinx.jpg" alt="sphinx" />
@@ -32,29 +47,54 @@ function Gallery(){
                         </div>
                     </div>
                 </div>
+                <div className="list-item">
+                    <img src="./images/david.webp" alt="sphinx" />
+                    <div className="item-content">
+                        <div className="item-author">Autor</div>
+                        <div className="item-title">Titulo</div>
+                        <div className="item-category">Categoria</div>
+                        <div className="item-description">
+                            Esto es un texto de prueba para la descripcion.
+                        </div>
+                        <div className="buttons">
+                            <button>SEE MORE</button>
+                        </div>
+                    </div>
+                </div>
                 <div className="thumbnail">
-                        <div className="list-item">
-                            <img src="./images/sphinx.jpg" alt="sphinx"/>
-                            <div className="item-content">
-                                <div className="item-title">
-                                    TITLE
-                                </div>
-                                <div className="item-description">
-                                    Descripcion
-                                </div>
+                    <div className="list-item">
+                        <img src="./images/sphinx.jpg" alt="sphinx" />
+                        <div className="item-content">
+                            <div className="item-title">
+                                TITLE
+                            </div>
+                            <div className="item-description">
+                                Descripcion
                             </div>
                         </div>
-                        <div className="list-item">
-                            <img src="./images/victoria.png" alt="victoria"/>
-                            <div className="item-content">
-                                <div className="item-title">
-                                    TITLE
-                                </div>
-                                <div className="item-description">
-                                    Descripcion
-                                </div>
+                    </div>
+                    <div className="list-item">
+                        <img src="./images/victoria.png" alt="victoria" />
+                        <div className="item-content">
+                            <div className="item-title">
+                                TITLE
+                            </div>
+                            <div className="item-description">
+                                Descripcion
                             </div>
                         </div>
+                    </div>
+                    <div className="list-item">
+                        <img src="./images/david.webp" alt="sphinx" />
+                        <div className="item-content">
+                            <div className="item-title">
+                                TITLE
+                            </div>
+                            <div className="item-description">
+                                Descripcion
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 <div className="arrows">
                     <button id="prev">{'<'}</button>
