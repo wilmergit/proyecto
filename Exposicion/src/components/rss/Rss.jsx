f6f9767d6f89ed1f7f74389947ce176ad7713de6
wilmergit <wilmerzeasrodriguez@gmail.com>
2024-05-07 20:37:29 +0100

    rss_progress

--- Exposicion/src/components/rss/Rss.jsx
+++ Exposicion/src/components/rss/Rss.jsx
@@ -1,63 +1,67 @@
 function Rss() {
     return (
-
+        
         <rss version="2.0">
             <channel>
-                <title>Juegos mas populares</title>
-                <description>Dedicated section to highlight the most 
-                    popular games from this week</description>
-                <link>https://as.com/meristation/juegos/top/?omnil=mpal</link>
+                <title>Juegos mas populares del 2024</title>
+                <description>Dedicated section to highlight the most
+                    popular games from this year</description>
+                <link>https://as.com/meristation/juegos/top/2024/</link>
                 <item>
-                    <!-- Titulo del elemento del feed -->
-                    <title>Prueba Final</title>
-                    <!-- Descripcion de la actualizacion del sitio que este elemento representa, puede contener cuanto texto e imagenes necesite-->
-                    <description>Descripción detallada de los elementos de un Feed RSS</description>
-                    <!-- Enlace a la seccion del sitio-->
-                    <link>https://ejemplo-rss.glitch.me/</link>
-                    <!-- Fecha de publicación -->
-                    <pubDate>Mon, 30 Dic 2019 23:00:00 EST</pubDate>
+                    <title>Prince of Persia: The lost crown</title>
+                    <description>Viaja por la mitología persa y manipula los límites del espacio y el tiempo.
+                        Usa tus poderes del tiempo, habilidades de combate y movimientos para ejecutar combos
+                        letales y derrotar a criaturas mitológicas y enemigos corrompidos por el tiempo.
+                    </description>
+                    <link>https://as.com/meristation/juegos/prince-of-persia-the-lost-crown/</link>
                 </item>
                 <item>
-                    <title>Demostración</title>
-                    <description>Abriremos nuestro sitio web desde un lector RSS y veamos que pasa</description>
-                    <link>https://ejemplo-rss.glitch.me/</link>
-                    <pubDate>Sun, 29 Dic 2019 15:00:00 EST</pubDate>
+                    <title>Final Fantasy VII Rebirth</title>
+                    <description>Final Fantasy VII Rebirth es un videojuego de acción RPG a cargo de Square Enix 
+                        para PlaySation 5 que continúa con la historia de Final Fantasy VII Remake. La travesía 
+                        desconocida continúa… Tras escapar de la distópica ciudad de Midgar, Cloud y sus amigos 
+                        se embarcan en un viaje por todo el planeta. Les aguardan nuevas aventuras en vasto y 
+                        vibrante mundo.
+                    </description>
+                    <link>https://as.com/meristation/juegos/final-fantasy-vii-rebirth/</link>
                 </item>
                 <item>
-                    <title>Enlazar el feed desde el encabezado HTML</title>
-                    <description>Como enlazar el feed desde el encabezado de modo que los lectores RSS lo puedan localizar</description>
-                    <link>https://ejemplo-rss.glitch.me/</link>
-                    <pubDate>Sat, 28 Dic 2019 15:00:00 EST</pubDate>
+                    <title>Like a Dragon: Infinite Wealth</title>
+                    <description>Like a Dragon: Infinite Wealth es un RPG con combates por turnos a cargo de 
+                        Ryu Ga Gotoku Studio y Sega para PC, PlayStation 4, Xbox One, PlayStation 5 y Xbox Series 
+                        que sigue con las aventuras de Ichiban Kasuga. Dos héroes más grandes que la vida misma se 
+                        unen guiados por el destino, o quizás, por algo más siniestro…
+                    </description>
+                    <link>https://as.com/meristation/juegos/like-a-dragon-8/</link>
                 </item>
                 <item>
-                    <title>Ejemplo de un Feed RSS</title>
-                    <description>Un ejemplo del codigo completo de un feed RSS</description>
-                    <link>https://ejemplo-rss.glitch.me/</link>
-                    <pubDate>Fri, 27 Dic 2019 15:00:00 EST</pubDate>
+                    <title>Dragon's Dogma 2</title>
+                    <description>Dragon's Dogma 2 es un videojuego de acción RPG a cargo de Capcom para PC, 
+                        PlayStation 5 y Xbox Series ambientado en el fantástico universo de la franquicia. 
+                        La historia arranca en una prisión subterránea donde el eco de la voz del dragón resuena 
+                        en la niebla de recuerdos perdidos.
+                    </description>
+                    <link>https://as.com/meristation/juegos/dragon-s-dogma-2/</link>
                 </item>
                 <item>
-                    <title>Multimedia en RSS</title>
+                    <title>Balatro</title>
                     <description><![CDATA[Ejemplo de como agregar una imagen al RSS, hacemos uso de CDATA para poner codigo HTML dentro del RSS, como la siguiente imagen <img src="https://cdn.glitch.com/6db38f9b-90fb-4ac8-a53a-110244e13c36%2Ffeedly.png?v=1577638836439" alt="Captura de pantalla de Feedly" />]]></description>
                     <link>https://ejemplo-rss.glitch.me/</link>
-                    <pubDate>Thu, 26 Dic 2019 14:00:00 EST</pubDate>
                 </item>
                 <item>
                     <title>Etiquetas de un Feed RSS</title>
                     <description>Que etiquetas forman un feed RSS</description>
                     <link>https://ejemplo-rss.glitch.me//</link>
-                    <pubDate>Wed, 25 Dic 2019 14:00:00 EST</pubDate>
                 </item>
                 <item>
                     <title>¿Que es un feed RSS?</title>
                     <description>Explicación de que es un Feed RSS</description>
                     <link>https://ejemplo-rss.glitch.me//</link>
-                    <pubDate>Thu, 24 Dic 2019 13:00:00 EST</pubDate>
                 </item>
                 <item>
                     <title>Anunciando tutorial de como hacer un RSS a mano</title>
                     <description>Anunciando tutorial de como hacer un RSS a mano</description>
                     <link>https://ejemplo-rss.glitch.me//</link>
-                    <pubDate>Mon, 23 Dic 2019 08:00:00 EST</pubDate>
                 </item>
             </channel>
         </rss>
