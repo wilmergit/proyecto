2d68fa22c1ad70bb1e146f4921fb88ec35df4606
wilmergit <wilmerzeasrodriguez@gmail.com>
2024-05-16 20:00:18 +0100

    final

--- Exposicion/src/App.jsx
+++ Exposicion/src/App.jsx
@@ -6,16 +6,17 @@ import Seller from './pages/seller/Seller'
 import MemberForm from './pages/memberForm/MemberForm'
 
 function App() {
+
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<Home/>} />
-        <Route path="/home" element={<Home/>} />
-        <Route path="/cookiesAndPrivacy" element={<PrivacyAndCookies/>} />
-        <Route path="/seller" element={<Seller/>} />
-        <Route path="/membership" element={<MemberForm/>} />
-      </Routes>
-    </BrowserRouter>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/home" element={<Home />} />
+          <Route path="/cookiesAndPrivacy" element={<PrivacyAndCookies />} />
+          <Route path="/seller" element={<Seller />} />
+          <Route path="/membership" element={<MemberForm />} />
+        </Routes>
+      </BrowserRouter>
   )
 }
 
